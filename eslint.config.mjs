aa7979d8ab9042eb6ad9a20413e035142a1061be
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2026-07-27 11:09:07 -0300

    Configure lint validation and remove reported warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig, globalIgnores } from "eslint/config";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+});
 
 const eslintConfig = defineConfig([
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
